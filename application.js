aa78df05ba03b3f63d785edb2d3993321ae27930
VJS <vjseraphim@gmail.com>
2021-02-10 19:15:13 +0900

    Colorize

--- application.js
+++ application.js
@@ -1,5 +1,7 @@
 const canvas = document.querySelector("#js-Canvas")
+const colors = document.getElementsByClassName("jsColor")
 const ctx = canvas.getContext("2d")
+const range = document.getElementById("js-range")
 
 canvas.width = 1080
 canvas.height = 720
@@ -33,10 +35,25 @@ function paintStop() {
     painting = false
 }
 
+function handleColor(event) {
+    const bgColor = event.target.style.backgroundColor
+    ctx.strokeStyle = bgColor
+
+}
+
+function handleRange(event) {
+    
+}
 
 if (canvas) {
     canvas.addEventListener("mousemove", onMouseMove)
     canvas.addEventListener("mousedown", paintStart)
     canvas.addEventListener("mouseup", paintStop)
     canvas.addEventListener("mouseleave", paintStop)
+}
+
+Array.from(colors).forEach(color => color.addEventListener("click", handleColor))
+
+if (range) {
+    range.addEventListener("input", handleRange)
 }
